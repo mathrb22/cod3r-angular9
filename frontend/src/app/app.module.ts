e9e3a5637cb2b959fb5889b06ee413ebad336c60
mathrb22 <mathribe2020@gmail.com>
2021-02-11 08:43:38 -0300

    Criação de views e declaração das rotas da aplicação

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -9,14 +10,26 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { MatCardModule } from '@angular/material/card';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 @NgModule({
-	declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		NavComponent,
+		HomeComponent,
+		ProductCrudComponent,
+	],
 	imports: [
 		BrowserModule,
+		AppRoutingModule,
 		BrowserAnimationsModule,
 		MatToolbarModule,
 		MatSidenavModule,
 		MatListModule,
+		MatCardModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
