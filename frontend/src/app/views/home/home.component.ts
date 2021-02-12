97e7f96223d56dea5ce64ed3a1a1d887f571dfdd
mathrb22 <mathribe2020@gmail.com>
2021-02-12 11:56:41 -0300

    Finalização da aplicação CRUD e alterações do cabeçalho

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,15 +1,19 @@
+import { HeaderService } from './../../components/template/header/header.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+	constructor(private headerService: HeaderService) {
+		headerService.headerData = {
+			title: 'Início',
+			icon: 'home',
+			routeUrl: '/products',
+		};
+	}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
